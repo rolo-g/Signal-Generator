d0e1d42a559a517f23eaacc9d744d429c412080e
rolo <rolando.rosales@pm.me>
2023-11-29 15:57:16 -0600

    Dc #4

--- wg.c
+++ wg.c
@@ -371,7 +371,7 @@ int main(int argc, char* argv[])
 
                     if (argc == 7)
                     {
-                        uint16_t dtycyc = 2045 / (100 / atoi(argv[6]));
+                        uint16_t dtycyc = 2045 * (uint16_t)(atoi(argv[6]) / 100);
                         printf("\n%d", dtycyc);
                         if (dtycyc >= 0 && dtycyc <= 2048)
                         {
